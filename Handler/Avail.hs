a89d60b3f69e91eab766e76ef0fe4d66103663a2
Alex Mason <axman6@gmail.com>
2014-06-06 22:30:34 +1000

    Added support for live changing of Availability info

--- Handler/Avail.hs
+++ Handler/Avail.hs
@@ -1,17 +1,40 @@
 module Handler.Avail where
 
 import Import
-import              Data.Time.Clock
-import              Data.Time.LocalTime
+import Data.Time.Clock
+import qualified Data.Map as M
+import Model.Availability
+import Handler.Users
+import Data.Text (pack)
 
 getAvailR :: Handler Html
 getAvailR = do
     now <- liftIO $ getCurrentTime
-    (es,us) <- runDB $ 
-        (,) <$> selectList
+    (es,us, rs) <- runDB $ do
+        es <- selectList
                     [EventDate >=. utctDay now]
                     [Asc EventDate, Asc EventTime, LimitTo 10]
-            <*> selectList [] [Asc UserFirstname]
+        us <- selectList [] [Asc UserFirstname]
+        rs <- selectList [AvailabilityEvent <-. map entityKey es] []
+        return (es,us,rs)
+    let mp = M.fromList $ map (\(Entity _ (Availability u e st note)) -> ((u,e),(st,note))) rs
     defaultLayout $ do
         setTitle "Availability"
         $(widgetFile "availability")
+
+
+postToggleAvailR :: UserId -> EventId -> Handler Text
+postToggleAvailR uid eid = do
+    res <- runDB $ do
+        ma <- getBy $ UniqueAvailability uid eid
+        case ma of
+            Nothing -> do
+                _ <- insert $ Availability uid eid Yes Nothing
+                return Yes
+            Just (Entity aid a) -> do
+                let newStatus = toggleAvailability (availabilityStatus a)
+                update aid
+                    [AvailabilityStatus =. newStatus]
+                return newStatus
+    return . pack . show $ res
+
--- Model/Availability.hs
+++ Model/Availability.hs
@@ -1,13 +1,21 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Model.Availability
     (
-    Available (..)
+      Available (..)
+    , toggleAvailability
     )
  where
 
 import Database.Persist.TH
 import Prelude (Show, Read, Eq)
 
-data Available = Yes | No | Unsure deriving (Show, Read, Eq)
+data Available = Unset | Yes | No | Unsure deriving (Show, Read, Eq)
+
+toggleAvailability :: Available -> Available
+toggleAvailability a = case a of
+    Unset -> Yes
+    Yes -> No
+    No -> Unsure
+    Unsure -> Unset 
 
 derivePersistField "Available"
