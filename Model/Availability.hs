03fecf1d9529b7abf0ba7b0ef23b097657b7ebfa
Alex Mason <axman6@gmail.com>
2014-06-08 13:14:00 +1000

    Refactoring display code for the Available (and new Period) types.
    MVC whaaat?

--- Model/Availability.hs
+++ Model/Availability.hs
@@ -2,21 +2,56 @@
 module Model.Availability
     (
       Available (..)
+    , Period (..)
     , toggleAvailability
+    , availableToText
+    , availableToClass
     )
  where
 
+
 import Database.Persist.TH
-import Prelude (Show, Read, Eq, Ord)
+import Yesod.Core.Dispatch
+import Prelude (Show, Read, Eq, Ord, Maybe(..), ($))
+import Data.Text (Text)
+import Text.Blaze (preEscapedToMarkup)
+import Text.Blaze.Html5 (Html)
 
 data Available = Unset | Yes | No | Unsure 
     deriving (Show, Read, Eq, Ord)
 
+data Period = Day | Night
+    deriving (Show, Read, Eq, Ord)
+
+instance PathPiece Period where
+    fromPathPiece t = case t of
+        "Day" -> Just Day
+        "Night" -> Just Night
+        _ -> Nothing
+    toPathPiece Day = "Day"
+    toPathPiece Night = "Night"
+
+
 toggleAvailability :: Available -> Available
 toggleAvailability a = case a of
-    Unset -> Yes
-    Yes -> No
-    No -> Unsure
-    Unsure -> Unset 
+    Unset   -> Yes
+    Yes     -> No
+    No      -> Unsure
+    Unsure  -> Unset 
+
+availableToText :: Available -> Html
+availableToText a = preEscapedToMarkup $ case a of
+    Yes     -> ("Yes" :: Text)
+    No      -> "No"
+    Unsure  -> "Unsure"
+    Unset   -> "&nbsp;&nbsp;&nbsp;"
+
+availableToClass :: Available -> Text
+availableToClass a = case a of
+    Yes     ->"btn-success"
+    No      ->"btn-danger" 
+    Unsure  ->"btn-warning"
+    Unset   ->"btn-default"
 
 derivePersistField "Available"
+derivePersistField "Period"
