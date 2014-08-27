a0bd98d44d185dc4945f322674ef0054d5668051
Alex Mason <axman6@gmail.com>
2014-08-27 22:37:49 +1000

    Added FullCalendar jQuery plugin

--- Handler/Calendar.hs
+++ Handler/Calendar.hs
@@ -10,41 +10,32 @@ import System.Locale (defaultTimeLocale)
 
 getCalendarR :: Handler Html
 getCalendarR = do
-    now <- liftIO $ getCurrentTime
-    let today = utctDay now
-        (year, month, _day) = toGregorian today
-        (yearDelta, nextMon) = (month + 1) `quotRem` 12
-        nextMonth = fromGregorian (year + fromIntegral yearDelta) nextMon 1
+    --now <- liftIO $ getCurrentTime
+    --let today = utctDay now
+    --    (year, month, _day) = toGregorian today
+    --    (yearDelta, nextMon) = (month + 1) `quotRem` 12
+    --    nextMonth = fromGregorian (year + fromIntegral yearDelta) nextMon 1
 
-        firstOfTheMonth = fromGregorian year month 1
-        (yr, wk, _) = toWeekDate firstOfTheMonth
-        prevSunday  = fromWeekDate yr wk 0
-        (nyr, nwk, _) = toWeekDate nextMonth
-        finalSaturday = fromWeekDate nyr nwk 6
+    --    firstOfTheMonth = fromGregorian year month 1
+    --    (yr, wk, _) = toWeekDate firstOfTheMonth
+    --    prevSunday  = fromWeekDate yr wk 0
+    --    (nyr, nwk, _) = toWeekDate nextMonth
+    --    finalSaturday = fromWeekDate nyr nwk 6
 
-        weeks = chunksOf 7 $ map (formatTime defaultTimeLocale "%a %b %e") 
-                            [prevSunday .. finalSaturday]
+    --    weeks = chunksOf 7 $ map (formatTime defaultTimeLocale "%a %b %e") 
+    --                        [prevSunday .. finalSaturday]
 
 
-    es' <- runDB $ selectList [EventDate >=. prevSunday, EventDate <=. finalSaturday]
-                              [Asc EventDate, Desc EventTime]
+    --es' <- runDB $ selectList [EventDate >=. prevSunday, EventDate <=. finalSaturday]
+    --                          [Asc EventDate, Desc EventTime]
     --es <- mapM (\u -> (u,) <$> newIdent) es'
     -- let mus = listToMaybe us
     --(formWidget, formEnctype) <- generateFormPost eventForm
+    
+
     defaultLayout $ do
         setTitle "Calendar"
-        [whamlet|
-            <p>
-                #{show prevSunday}
-            <p>
-                #{show finalSaturday}
-            <table>
-                $forall week <- weeks
-                    <tr>
-                    $forall day <- week
-                        <td>
-                            <p>#{day}
-                
-        |]
-
-        -- $(widgetFile "events")
+        addScript     $ StaticR js_moment_with_locales_js
+        addScript     $ StaticR js_fullcalendar_2_1_0_fullcalendar_js
+        addStylesheet $ StaticR js_fullcalendar_2_1_0_fullcalendar_css
+        $(widgetFile "calendar")
